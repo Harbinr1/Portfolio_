42f9fb93f9f7ac3f9231ed668981a9bb2301e582
Harbinr1 <55412732+Harbinr1@users.noreply.github.com>
2022-05-16 15:36:34 +0200

    deploy my react app to github pages

---
+++ src/components/Acomplishments/Acomplishments.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import {dataAcomplishment} from "../../constants/constants"
+
+
+const Acomplishments = () => (
+  <Section>
+      
+       <Boxes>
+       
+       </Boxes>
+  </Section>
+);
+
+export default Acomplishments;
